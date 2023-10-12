aef700eb4e8912ec1b0e39cf7e434152b318c638
rajput-karsh <utkarsh1@appinventiv.com>
2023-10-12 10:58:01 +0530

    reset env

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+      const response = await fetch(`${import.meta.API_BASE_URL}/api/v1/post`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
